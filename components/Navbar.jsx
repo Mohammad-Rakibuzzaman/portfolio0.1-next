d52569cf0b749998b4c7efa95b318451ee3de1a4
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-10-05 19:50:05 +0600

    header designed successfully

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,10 @@ import Image from 'next/image'
 import React from 'react'
 import logo from '../public/assets/navLogo.png'
 import Link from 'next/link'
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
+import { FaGithub, FaLinkedin, FaLinkedinIn } from 'react-icons/fa'
+import { BsPersonLinesFill } from 'react-icons/bs'
+import { FaX } from 'react-icons/fa6'
 
 const Navbar = () => {
   return (
@@ -41,16 +44,73 @@ const Navbar = () => {
 
       <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
         <div className='fixed left-0 top-0 w-[75%] sm:w-[65%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+          
           <div className='flex w-full items-end justify-end'>
             <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
               <AiOutlineClose />
             </div>
           </div>
 
+          <div className='border-b border-gray-300 my-4'>
+              <p className='w-[85%] md:w-[90%] py-4'>Empowering Innovation with Custom AI Solutions</p>
+          </div>
+          
+          <div className='py-4 flex flex-col'>
+            <ul className='uppercase'>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Home</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>About</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Skills</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Projects</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Blog</li>
+              </Link>
+              <Link href='/'>
+                <li className='py-4 text-sm'>Contact</li>
+              </Link>
+            </ul>
+            
+            <div className='pt-40'>
+
+              <p className='uppercase tracking-widest text-[#5651e5]'>Let's Connect</p>
+              
+              <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                  <FaLinkedinIn />  
+                </div> 
+                
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                  <AiOutlineMail />  
+                </div> 
+                 
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                  <FaGithub />  
+                </div>  
+                 
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                  <FaX />  
+                </div>  
+
+              </div>
+
+              
+            </div>
+
+
+
+          </div>
+
+
+
         </div>
-
-
-
       </div>
 
 
